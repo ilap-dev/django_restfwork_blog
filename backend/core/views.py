aa86a099e23f00e4806fbe8c11d1ebea3f9d5bc1
ilap42 <ilap435@gmail.com>
2025-03-28 10:24:03 -0600

    Initial first Commit

---
+++ backend/core/views.py
@@ -0,0 +1,8 @@
+
+##Forma mas basica de crear un API
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class TestView(APIView):
+    def get(self,request,*args, **kwargs):
+        return Response("Hola Mundo")
